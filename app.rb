3a8d79d6c36f41e8c039040c816df3e8a3482983
Dommmel <dommmel@gmail.com>
2014-03-19 23:22:08 +0200

    add canonical

--- app.rb
+++ app.rb
@@ -4,17 +4,17 @@ not_found do
   'That time is nowhere to be found.'
 end
 
-get "/:time_standard/:time" do
+get "/:time/:time_standard" do
   begin
     d = DateTime.parse(params[:time]) 
   rescue
     halt 404 
   end
-  time = d.strftime("%H:%M:%S")
-  time_standard = params[:time_standard].upcase
-  title = params[:time] + " " + time_standard
-  if ["GMT", "UTC"].include? time_standard
-    erb :index, locals: { title: title, time: time, time_standard: time_standard }
+  @time = d.strftime("%H:%M:%S")
+  @time_standard = params[:time_standard].upcase
+  @title = params[:time] + " " + @time_standard
+  if ["GMT", "UTC"].include? @time_standard
+    erb :index
   else
     halt 404
   end
