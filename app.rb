c7de8c08639d8f4976790c0e33cbcdb263b72518
Dommmel <dommmel@gmail.com>
2014-03-20 03:32:34 +0200

    redirects

--- app.rb
+++ app.rb
@@ -10,19 +10,33 @@ get "/" do
 end
 
 get "/:time/:time_standard" do
+
+  parsable_timezones = ["GMT", "UTC", "UT", "PST", "PDT", "EST", "EDT", "CST", "CDT", "MST", "MDT"]
+  
+
+  # redirect from /timezone/time to /time/timezone
+  begin
+    d = DateTime.parse(params[:time_standard])
+    if parsable_timezones.include? params[:time].upcase
+      redirect to("/#{params[:time_standard]}/#{params[:time]}"), 301
+    end
+  rescue
+  end
+
+  # Parse date
   begin
     d = DateTime.parse(params[:time]) 
   rescue
     halt 404 
   end
-  
+
   @time = d.strftime("%H:%M:%S")
   @time_standard = params[:time_standard].upcase
 
   @today_string = Time.now.strftime("%Y/%m/%d")
   @title = params[:time] + " " + @time_standard
   @page_title = @title + " in local time (your timezone)"
-  if ["GMT", "UTC", "UT", "PST", "PDT", "EST", "EDT", "CST", "CDT", "MST", "MDT"].include? @time_standard
+  if parsable_timezones.include? @time_standard
     erb :index
   else
     halt 404
